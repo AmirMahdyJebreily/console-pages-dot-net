217426f5037a605456c103bb7b4da5bbe6b6de62
Amir JCS <amirmahdyjdx@gmail.com>
2023-08-29 23:46:00 +0330

    docs(IPage.cs file): add missed xml documents

--- src/Models/IPage.cs
+++ src/Models/IPage.cs
@@ -1,8 +1,18 @@
 ﻿namespace ConsolePages
 {
+    /// <summary>
+    /// base of all pages
+    /// </summary>
     public interface IPage
     {
+        /// <summary>
+        /// Layout of content in this page. It is usually copied from CApp data
+        /// </summary>
         IAppContentLayout ContentLayout { get; }
+
+        /// <summary>
+        /// Main text content of this page
+        /// </summary>
         string MainContent { get; set; }
     }
 }
