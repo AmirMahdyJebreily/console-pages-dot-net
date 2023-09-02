dfc0b0ffa0724ac18d83a68f9cd583ad41ab548b
Amir JCS <amirmahdyjdx@gmail.com>
2023-09-03 02:33:57 +0330

    test: test pages structures

--- tests/PagesTest/Program.cs
+++ tests/PagesTest/Program.cs
@@ -6,6 +6,7 @@ Page p = new Page(cApp.Layout);
 
 p.DefineDialog((args) =>
 {
-    args.Print(args); // Incorrect format 
-    // I'll fix it when I get back from going out for my mom's shopping!
+    args.Print("Hello World"); 
 });
+
+p.Show();
