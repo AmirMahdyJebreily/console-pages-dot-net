22c96888382fc10c52823aed42a794c9a51f53ec
Amir JCS <amirmahdyjdx@gmail.com>
2023-09-03 01:30:46 +0330

    test: structures tests

--- tests/PagesTest/Program.cs
+++ tests/PagesTest/Program.cs
@@ -1,3 +1,11 @@
 ﻿using ConsolePages;
 
 CApp cApp = new CApp();
+
+Page p = new Page(cApp.Layout);
+
+p.DefineDialog((args) =>
+{
+    args.Print(args); // Incorrect format 
+    // I'll fix it when I get back from going out for my mom's shopping!
+});
