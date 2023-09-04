8a127b1459b5e220b032f834403612237e03d2b4
Amir JCS <amirmahdyjdx@gmail.com>
2023-09-04 22:04:11 +0330

    refactor: rename "Print" and "PrintLn" methods

--- src/Models/Pages/PagesArgs.cs
+++ src/Models/Pages/PagesArgs.cs
@@ -10,89 +10,89 @@ namespace ConsolePages
         private ColorTheme _theme = _prvt_sttc._dflt_color_theme;
 
         #region Print Method
-        public void Print(object obj) => _print(obj);
-        public void Print(string text) => _print(text);
-        public void Print(string text, ConsoleColor color)
+        public void PrintNl(object obj) => _print(obj);
+        public void PrintNl(string text) => _print(text);
+        public void PrintNl(string text, ConsoleColor color)
         {
             _ch_color(color);
             _print(text);
             _ch_color(_theme.PrimeryColor);
 
         }
-        public void Print(string sep = " ", params string[] texts) => Print(string.Join(sep, texts));
-        public void Print(params string[] texts) => Print(string.Join(" ", texts));
-        public void Print(string sep = " ", params (string, ConsoleColor)[] texts)
+        public void PrintNl(string sep = " ", params string[] texts) => PrintNl(string.Join(sep, texts));
+        public void PrintNl(params string[] texts) => PrintNl(string.Join(" ", texts));
+        public void PrintNl(string sep = " ", params (string, ConsoleColor)[] texts)
         {
             for (int i = 0; i < texts.Length; i++)
             {
                 _ch_color(texts[i].Item2);
-                Print(texts[i].Item1);
+                PrintNl(texts[i].Item1);
                 _ch_color(_theme.PrimeryColor);
 
                 if (i + 1 != texts.Length)
                 {
-                    Print(sep); // add seprator to texts
+                    PrintNl(sep); // add seprator to texts
                 }
 
             }
         }
-        public void Print(params (string, ConsoleColor)[] texts)
+        public void PrintNl(params (string, ConsoleColor)[] texts)
         {
             for (int i = 0; i < texts.Length; i++)
             {
                 _ch_color(texts[i].Item2);
-                Print(texts[i].Item1);
+                PrintNl(texts[i].Item1);
                 _ch_color(_theme.PrimeryColor);
 
                 if (i + 1 != texts.Length)
                 {
-                    Print(" "); // add seprator to texts
+                    PrintNl(" "); // add seprator to texts
                 }
 
             }
         }
         #endregion
 
-        #region PrintLn Method
-        public void PrintLn(object obj) => Print(obj.ToString(), "\n");
-        public void PrintLn(string text) => PrintLn(text);
-        public void PrintLn(string text, ConsoleColor color)
+        #region Print Method
+        public void Print(object obj) => PrintNl(obj.ToString(), "\n");
+        public void Print(string text) => Print(text);
+        public void Print(string text, ConsoleColor color)
         {
             _ch_color(color);
-            Print(text, "\n");
+            PrintNl(text, "\n");
             _ch_color(_theme.PrimeryColor);
 
         }
-        public void PrintLn(string[] texts, string sep = "\n") => Print(string.Join(sep, texts));
-        public void PrintLn(params string[] texts) => Print(string.Join("\n", texts));
+        public void Print(string[] texts, string sep = "\n") => PrintNl(string.Join(sep, texts));
+        public void Print(params string[] texts) => PrintNl(string.Join("\n", texts));
 
-        public void PrintLn((string, ConsoleColor)[] texts, string sep = "\n")
+        public void Print((string, ConsoleColor)[] texts, string sep = "\n")
         {
             for (int i = 0; i < texts.Length; i++)
             {
                 _ch_color(texts[i].Item2);
-                Print(texts[i].Item1);
+                PrintNl(texts[i].Item1);
                 _ch_color(_theme.PrimeryColor);
 
                 if (i + 1 != texts.Length)
                 {
-                    Print(sep); // add seprator to texts
+                    PrintNl(sep); // add seprator to texts
                 }
 
             }
         }
 
-        public void PrintLn(params (string, ConsoleColor)[] texts)
+        public void Print(params (string, ConsoleColor)[] texts)
         {
             for (int i = 0; i < texts.Length; i++)
             {
                 _ch_color(texts[i].Item2);
-                Print(texts[i].Item1);
+                PrintNl(texts[i].Item1);
                 _ch_color(_theme.PrimeryColor);
 
                 if (i + 1 != texts.Length)
                 {
-                    Print("\n"); // add seprator to texts
+                    PrintNl("\n"); // add seprator to texts
                 }
 
             }
