35849ae151358883fffb39bdc4eb477fa7d94380
Amir JCS <amirmahdyjdx@gmail.com>
2023-09-05 00:52:25 +0330

    test: new syntax test

--- tests/PagesTest/Program.cs
+++ tests/PagesTest/Program.cs
@@ -6,6 +6,6 @@ cApp.ShowPage(new Page(cApp.Layout, (a) =>
 {
     a.Print(("[Error]", a.ErrorColor), "An error occurred in the code...");
     a.Print(("[Success]", a.SuccessColor), "Download Successfully completed...");
-    a.Print(("[Info]", a.InfoColor), "App version is 0.0.1 ...");
+    a.Print(("[Info]", a.InfoColor), "App version is", ("0.0.1", a.WarningColor), "...");
     a.Print("You have", ("12$", a.InfoColor), "in your wallet");
 }));
