0e0676613320a354bc84ca4f251d8d3843bce540
Amir JCS <amirmahdyjdx@gmail.com>
2023-09-04 22:47:21 +0330

    test: test new syntax

--- tests/PagesTest/Program.cs
+++ tests/PagesTest/Program.cs
@@ -2,11 +2,10 @@
 
 CApp cApp = new CApp();
 
-Page p = new Page(cApp.Layout);
-
-p.DefineDialog((args) =>
+cApp.ShowPage(new Page(cApp.Layout, (a) =>
 {
-    args.Print(("[Error]",args.ErrorColor), ("an error occurred in code...", args.PrimeryColor));
-});
-
-p.Show();
+    a.Print(("[Error]", a.ErrorColor), ("An error occurred in the code...", a.PrimeColor));
+    a.Print(("[Success]", a.SuccessColor), ("Download Successfully completed...", a.PrimeColor));
+    a.Print(("[Info]", a.InfoColor), ("App version is 0.0.1 ...", a.PrimeColor));
+    a.Print(("You have", a.PrimeColor), ("12$", a.InfoColor), ("in your wallet",a.PrimeColor));
+}));
