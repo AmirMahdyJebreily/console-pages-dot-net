f8508df59c01967019f9272e3b57b46bcede0468
Amir JCS <amirmahdyjdx@gmail.com>
2023-09-06 02:40:02 +0330

    pref: completion of object-oriented relationships

--- src/Models/IPage.cs
+++ src/Models/IPage.cs
@@ -5,17 +5,12 @@ namespace ConsolePages
     /// <summary>
     /// base of all pages
     /// </summary>
-    public interface IPage
+    public interface IPage : IAppContentLayout
     {
         /// <summary>
         /// Layout of content in this page. It is usually copied from CApp data
         /// </summary>
         IAppContentLayout ContentLayout { get; }
-
-        /// <summary>
-        /// Main text content of this page
-        /// </summary>
-        Action<DialogStream> DialogStream { get; }
 
         IPage Show();
     }
