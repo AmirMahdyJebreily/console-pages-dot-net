041ba35fc810dd3813ac804cfbfbc3cc3ffc901f
Amir JCS <amirmahdyjdx@gmail.com>
2023-09-06 02:15:27 +0330

    test: input method test

--- tests/PagesTest/Program.cs
+++ tests/PagesTest/Program.cs
@@ -2,10 +2,13 @@
 
 CApp cApp = new CApp();
 
+string txt = string.Empty;
+
 cApp.ShowPage(new Page(cApp.Layout, (a) =>
 {
     a.Print(("[Error]", a.ErrorColor), "An error occurred in the code...");
     a.Print(("[Success]", a.SuccessColor), "Download Successfully completed...");
     a.Print(("[Info]", a.InfoColor), "App version is", ("0.0.1", a.WarningColor), "...");
-    a.Print("You have", ("12$", a.InfoColor), "in your wallet");
+    txt = a.Input("Enter your",("age",a.InfoColor),": ");
+    a.Print("Your age is", (txt, a.InfoColor));
 }));
